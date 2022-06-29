3098943933b5e90d3c30cac211529e888a025d98
sitebix <sitebix@gmail.com>
2022-06-29 15:21:39 -0500

    updated firebase rules for hosting

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "604d171cb13ee2ad"; }
+/******/ 		__webpack_require__.h = function() { return "985d583b1218e4e4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
